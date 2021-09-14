28c8d801b9926650afed9f4d951de29d00b28832
Jayme Coulton <coultonjayme@yahoo.com>
2021-09-14 13:51:26 -0500

    added functionality

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ let rollbar = new Rollbar({
 	captureUnhandledRejections: true,
 });
 
+const students = [];
 const app = express();
 
 app.use(express.json());
@@ -15,7 +16,29 @@ app.use("/style", express.static("./public/styles.css"));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "/public/index.html"));
-	rollbar.info("HTML file served sucessfully!");
+	rollbar.info("HTML file served successfully!");
+});
+
+app.post("/api/student", (req, res) => {
+	let { name } = req.body;
+	name = name.trim();
+
+	const index = students.findIndex((studentName) => studentName === name);
+
+	if (index === -1 && name !== "") {
+		students.push(name);
+		rollbar.log("Student added successfully", {
+			author: "Jayme",
+			type: "manual entry",
+		});
+		res.status(200).send(students);
+	} else if (name === "") {
+		rollbar.error("No name given");
+		res.status(400).send("must provide a name.");
+	} else {
+		rollbar.error("student already exists");
+		res.status(400).send("that student already exists");
+	}
 });
 
 app.get("/error", (req, res) => {
